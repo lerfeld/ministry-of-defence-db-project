cb3d3bc4f9c14641396e0c1b5add12eb612cc289
lerfeld <ly@sem-wolf.org>
2020-07-28 05:30:09 +0300

    realization create Database

---
+++ db.py
@@ -0,0 +1,161 @@
+import csv
+from typing import Type, List, Any, Dict
+import os
+import db_api
+
+
+class DBField(db_api.DBField):
+    name: str
+    type: Type
+
+
+class SelectionCriteria(db_api.SelectionCriteria):
+    field_name: str
+    operator: str
+    value: Any
+
+
+class DBTable(db_api.DBTable):
+    name: str
+    fields: List[DBField]
+    key_field_name: str
+
+    def get_fields_name(self):
+        return [field.name for field in self.fields]
+
+    def get_index_of_field(self, field):
+        return self.get_fields_name().index(field)
+
+    def count(self) -> int:
+        return DataBase.__DICT_TABLE__[self.name]["count"]
+
+    def insert_record(self, values: Dict[str, Any]) -> None:
+        if self.key_field_name not in values.keys():
+            raise KeyError
+        with open(f"db_files/{self.name}.csv", 'r') as db_table:
+            reader = csv.reader(db_table)
+            next(reader)
+            key_index = self.get_index_of_field(self.key_field_name)
+            for record in reader:
+                if record:
+                    if record[key_index] == values[self.key_field_name]:
+                        raise KeyError
+        with open(f"db_files/{self.name}.csv", 'a', newline='') as db_table:
+            row = [values[field.name] for field in self.fields]
+            writer = csv.writer(db_table)
+            writer.writerow(row)
+        DataBase.__DICT_TABLE__[self.name]["count"] += 1
+
+    def delete_record(self, key: Any) -> None:
+        with open(f"db_files/{self.name}.csv", 'r') as db_table:
+            reader = csv.reader(db_table)
+            next(reader)
+            key_index = self.get_index_of_field(self.key_field_name)
+            clean_rows = [record for record in reader if record and record[key_index] != key]
+        if clean_rows:
+            with open(f"db_files/{self.name}.csv", 'w') as db_table:
+                writer = csv.writer(db_table)
+                writer.writerow(clean_rows)
+            DataBase.__DICT_TABLE__[self.name]["count"] -= 1
+        else:
+            raise ValueError
+
+    def delete_records(self, criteria: List[SelectionCriteria]) -> None:
+        with open(f"db_files/{self.name}.csv", 'r') as db_table:
+            reader = csv.reader(db_table)
+            next(reader)
+        clean_rows = []
+        for row in reader:
+            for condition in criteria:
+                key_index = self.get_index_of_field(condition.field_name)
+                condition_ = row[key_index] + condition.operator + str(condition.value)
+                if not eval(condition_):
+                    clean_rows += row
+                    continue
+        if clean_rows:
+            DataBase.__DICT_TABLE__[self.name]["count"] = len(reader) - len(clean_rows)
+            with open(f"db_files/{self.name}.csv", 'w') as db_table:
+                writer = csv.writer(db_table)
+                writer.writerow(clean_rows)
+
+    def get_record(self, key: Any) -> Dict[str, Any]:
+        with open(f"db_files/{self.name}.csv", 'r') as db_table:
+            reader = csv.reader(db_table)
+            next(reader)
+            key_index = self.get_index_of_field(self.key_field_name)
+            get_dict = {}
+            for record in reader:
+                if record and record[key_index] == str(key):
+                    for field in self.field.name():
+                        get_dict[field] = record[self.get_index_of_field(field)]
+                    return get_dict
+        raise ValueError("this is error")
+
+    def update_record(self, key: Any, values: Dict[str, Any]) -> None:
+        with open(f"db_files/{self.name}.csv", 'r') as db_table:
+            reader = csv.reader(db_table)
+            next(reader)
+            key_index = self.get_index_of_field(self.key_field_name)
+            update_rows = []
+            for record in reader:
+                if record and record[key_index] == str(key):
+                    row = record
+                else:
+                    update_rows += record
+            for name_field in [field.name for field in self.fields]:
+                if name_field in values.keys():
+                    row[self.get_index_of_field(name_field)] = values[name_field]
+            update_rows += row
+        with open(f"db_files/{self.name}.csv", 'w') as db_table:
+            writer = csv.writer(db_table)
+            writer.writerow(update_rows)
+
+    def query_table(self, criteria: List[SelectionCriteria]) \
+            -> List[Dict[str, Any]]:
+        raise NotImplementedError
+
+    def create_index(self, field_to_index: str) -> None:
+        raise NotImplementedError
+
+
+class DataBase(db_api.DataBase):
+    __DICT_TABLE__ = {}
+
+    def create_table(self,
+                        table_name: str,
+                        fields: List[DBField],
+                        key_field_name: str) -> DBTable:
+        if table_name in self.__DICT_TABLE__.keys():
+            raise NameError
+        with open(f"db_files/{table_name}.csv", 'w') as data_table:
+            writer = csv.writer(data_table)
+            writer.writerow([field.name for field in fields])
+        db_table = DBTable(table_name, fields, key_field_name)
+        DataBase.__DICT_TABLE__[table_name] = {"table": db_table, "count": 0}
+        return db_table
+
+    def num_tables(self) -> int:
+        return len(self.__DICT_TABLE__.keys())
+
+    def get_table(self, table_name: str) -> DBTable:
+        return DataBase.__DICT_TABLE__[table_name].get("table")
+
+    def delete_table(self, table_name: str) -> None:
+        if table_name in DataBase.__DICT_TABLE__.keys():
+            os.remove(f"db_files/{table_name}.csv")
+            del self.__DICT_TABLE__[table_name]
+        else:
+            raise FileNotFoundError
+
+    def get_tables_names(self) -> List[Any]:
+        return list(DataBase.__DICT_TABLE__.keys())
+
+    def query_multiple_tables(
+            self,
+            tables: List[str],
+            fields_and_values_list: List[List[SelectionCriteria]],
+            fields_to_join_by: List[str]
+    ) -> List[Dict[str, Any]]:
+        raise NotImplementedError
+
+
